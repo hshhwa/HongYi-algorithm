db631f4cc03d96fa71d08f4e3eb9172dc4d77dd4
hshhwa <hshdla@naver.com>
2024-06-24 14:09:54 +0900

    p1008
    문제를 똑바로 읽도록 하자.

---
+++ Class1/p1008.java
@@ -0,0 +1,15 @@
+package Class1;
+
+import java.util.Scanner;
+
+public class p1008 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double A = sc.nextInt();
+		double B = sc.nextInt();
+		
+		System.out.println(A/B);
+	}
+
+}
